f886c24549b624ec2104ac23e21b28842355ad46
Przemek <antykwadrat@gmail.com>
2022-11-06 14:03:39 +0100

    feat(DB): add and configure the db

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
+## Prerequisites
+
+A running Docker container of Postgres. Following command can be used to make it work with the app's default DB config:
+```bash
+$ docker run \
+    --name postgres-product-crud \
+    -p 5433:5432 \
+    -e POSTGRES_DB=product-crud-db \
+    -e POSTGRES_USER=product-crud-user \
+    -e POSTGRES_PASSWORD=postgres \
+    -d postgres
+```
+
 ## Installation
 
 ```bash
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env.dev'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: process.env.NODE_ENV !== 'prod',
     }),
   ],
 })
