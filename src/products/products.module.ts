d7689a7078cc2cbebb0f9038d16aa599e271795e
Przemek <antykwadrat@gmail.com>
2022-11-07 15:06:18 +0100

    feat(Products): add blank ProductsController

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
+import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product])],
   providers: [ProductsService],
+  controllers: [ProductsController],
 })
 export class ProductsModule {}
