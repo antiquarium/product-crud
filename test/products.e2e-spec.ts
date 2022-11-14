3332b75895833dcbf232f0c9428650828ef49737
Przemek <antykwadrat@gmail.com>
2022-11-14 16:45:22 +0100

    test(Products): add e2e test for GET /products endpoint

--- test/products.e2e-spec.ts
+++ test/products.e2e-spec.ts
@@ -13,6 +13,7 @@ import { ProductsModule } from '../src/products/products.module';
 describe('ProductsController (e2e)', () => {
   let app: INestApplication;
   let productsRepository: Repository<Product>;
+  let server: any;
 
   beforeAll(async () => {
     ConfigModule.forRoot(getConfigModuleRootOpts());
@@ -25,6 +26,7 @@ describe('ProductsController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     productsRepository = app.get(getRepositoryToken(Product));
+    server = app.getHttpServer();
     await app.init();
   });
 
@@ -32,10 +34,29 @@ describe('ProductsController (e2e)', () => {
     await app.close();
   });
 
-  it('/products (GET)', async () => {
-    return request(app.getHttpServer())
-      .get('/products')
-      .expect(200)
-      .expect('[]');
+  describe('/products (GET)', () => {
+    it('responds with a list of all products', async () => {
+      const products: Product[] = [
+        {
+          Id: 'fed98f29-732d-40e5-98d2-19e1d61b9f6a',
+          Name: 'SUGDW apple',
+          Price: 500,
+          UpdateDate: new Date('2022-11-14T15:00:00.000Z'),
+        },
+        {
+          Id: '4532816c-2a2d-4d56-831e-f58727c350af',
+          Name: 'bamboo shoot',
+          Price: 50,
+          UpdateDate: new Date('2022-11-14T16:00:00.000Z'),
+        },
+      ];
+      await productsRepository.save(products);
+
+      const response = await request(server).get('/products');
+
+      expect(response.status).toEqual(200);
+      /* Product requires UpdateDate to be a Date, but API returns a string instead */
+      expect(response.body).toEqual(JSON.parse(JSON.stringify(products)));
+    });
   });
 });
